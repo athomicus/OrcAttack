f0c5c20c9d60bdf83469e08ebee672476af78dc6
athomicus <athomicus@gmail.com>
2025-12-11 07:54:27 +0100

    laddar managing

--- game_objects/ladder.go
+++ game_objects/ladder.go
@@ -1,11 +1,11 @@
 components {
-  id: "Orc"
-  component: "/main/Orc.script"
+  id: "ladder"
+  component: "/main/ladder.script"
 }
 embedded_components {
-  id: "sprite"
+  id: "ladder_sprite"
   type: "sprite"
-  data: "default_animation: \"walking\"\n"
+  data: "default_animation: \"ladder\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "textures {\n"
   "  sampler: \"texture_sampler\"\n"
@@ -25,43 +25,22 @@ embedded_components {
   "mass: 0.0\n"
   "friction: 0.1\n"
   "restitution: 0.5\n"
-  "group: \"orc\"\n"
-  "mask: \"ladder\"\n"
+  "group: \"ladder\"\n"
+  "mask: \"orc\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
   "    position {\n"
-  "      x: 6.0\n"
-  "      y: -1.0\n"
+  "      x: -2.0\n"
   "    }\n"
   "    rotation {\n"
   "    }\n"
   "    index: 0\n"
   "    count: 3\n"
   "  }\n"
-  "  data: 36.63831\n"
-  "  data: 43.46314\n"
+  "  data: 28.619425\n"
+  "  data: 71.346504\n"
   "  data: 10.0\n"
   "}\n"
   ""
-}
-embedded_components {
-  id: "sprite_fade"
-  type: "sprite"
-  data: "default_animation: \"walkToClimb\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "textures {\n"
-  "  sampler: \"texture_sampler\"\n"
-  "  texture: \"/assets/images/orc_enemy/Orc.atlas\"\n"
-  "}\n"
-  ""
-  position {
-    x: -11.0
-    y: 21.0
-  }
-  scale {
-    x: 0.6
-    y: 0.6
-    z: 0.6
-  }
 }
